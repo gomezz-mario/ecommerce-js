75d73063bcb65774cdcf4dfb2227b2b89e86f2cf
Mario Gomez <gomezz.mario.ar@gmail.com>
2022-07-13 12:21:59 -0300

    Implementación de Styled-components. Cambios en estructura de archivos. Nuevos estilos en Navbar.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-import Navbar from './componentes/Navbar/Navbar';
+import Navbar from './componentes/Navbar';
 
 function App() {
   return (
---
+++ src/componentes/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from 'styled-components';
+import { FaGlideG } from "react-icons/fa";
+
+export const Nabvar = () => {
+	return(
+		<NavbarContainer>
+			<h2><FaGlideG/> My ecommers</h2>
+			<ul className="nav__list">
+				<li><a className="nav__link" href="">Inicio</a></li>
+				<li><a className="nav__link" href="">Productos</a></li>
+				<li><a className="nav__link" href="">Carrito</a></li>
+			</ul>
+		</NavbarContainer>
+	);
+};
+
+export default Nabvar;
+
+const NavbarContainer = styled.nav`
+	h2{
+		margin-left: .4rem;
+		color: #FFF;
+	}
+
+	.nav__list {
+		display: flex;
+		justify-content: start;
+		align-items: center;
+		list-style: none;
+		margin-right: 4rem;
+	}
+	.nav__link{
+		display: flex;
+		align-items: center;
+		margin: 1rem;
+		text-decoration: none;
+		color: #FFF;
+	}
+
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: .4rem;
+	background-color: #333;
+`;
