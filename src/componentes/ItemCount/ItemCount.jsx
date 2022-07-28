0285845708e5825fe29e8e38870cec8c74d48144
Mario Gomez <gomezz.mario.ar@gmail.com>
2022-07-27 21:54:51 -0300

    Modificaciones en ItemCount y en ItemDetail

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./item_count.css";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
-export const ItemCount = ({count, onCount}) => {
+export const ItemCount = ({count, onCount, minCount, maxCount}) => {
+
+	
 	return(
-		<div>
-			<button onClick={onCount(count-1)}>-</button>
-			<span>{count}</span>
-			<button onClick={onCount(count+1)}>+</button>
+		<div className="item-count-w">
+			<div className="item-count__container">
+				<button disabled={(count == minCount)} onClick={()=> onCount(count-1)}><FaAngleLeft/></button>
+			</div>
+			
+			<div className="item-count__container item-count__container--count">
+				<span className="item-count__count">{count}</span>
+			</div>
+			<div className="item-count__container">
+				<button  disabled={(count == maxCount)} onClick={()=> onCount(count+1)}><FaAngleRight/></button>
+			</div>
 		</div>
 	);
 };
--- src/componentes/ItemDetail/itemDetail.jsx
+++ src/componentes/ItemDetail/itemDetail.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { useState } from "react";
+import ItemCount from "../ItemCount/ItemCount";
+
+
+import { FiShoppingCart } from "react-icons/fi";
+import { VscChevronLeft, VscChevronRight} from "react-icons/vsc";
 
 export const ItemDetail = ({data}) => {
-	console.log(data);
+	const [count, setCount] = useState(1);
+
 	return(
-		<div className="item-detail-wrapper">
-			<div className="item-detal__image-w">
-				<img src={data.images[0]} alt="imagen de producto" />
+		<div className="product">
+			<div className="">
+				<img src={data.images[0]} alt={data.name} />
 			</div>
-			<div className="item-detail__data-content">
-				<h1>{data.name}</h1>
-				<p>${data.price}</p>
+			<div className="">
+				<div className="">
+					<h1>{data.name}</h1>
+				</div>
+				<div className="price-and-info">
+					<span>${data.price}</span>
+				</div>
+				<div className="box-to-cart">
+					<ItemCount count={count} onCount={setCount} minCount={1} maxCount={data.stock}/>
+					<button>
+						<FiShoppingCart/>
+						<span>Agregar al carrito</span>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
