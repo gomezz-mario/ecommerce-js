c468997db4ce61ec4c608a3fcbabbfb717e522db
Mario Gomez <gomezz.mario.ar@gmail.com>
2022-07-28 01:32:47 -0300

    Aplicando estilos a ItemDetail

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import "./item_count.css";
-import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import { useState } from "react";
 
 export const ItemCount = ({count, onCount, minCount, maxCount}) => {
 
 	
+
+
 	return(
 		<div className="item-count-w">
-			<div className="item-count__container">
-				<button disabled={(count == minCount)} onClick={()=> onCount(count-1)}><FaAngleLeft/></button>
-			</div>
-			
-			<div className="item-count__container item-count__container--count">
+					
+			<div className="item-count">
+				<button disabled={(count == minCount)} onClick={()=> onCount(count-1)}><FaMinus/></button>
 				<span className="item-count__count">{count}</span>
+				<button  disabled={(count == maxCount)} onClick={()=> onCount(count+1)}><FaPlus/></button>
+				
 			</div>
-			<div className="item-count__container">
-				<button  disabled={(count == maxCount)} onClick={()=> onCount(count+1)}><FaAngleRight/></button>
-			</div>
+
+			
+
 		</div>
+
+		
 	);
 };
 
--- src/componentes/ItemDetail/itemDetail.jsx
+++ src/componentes/ItemDetail/itemDetail.jsx
@@ -1,33 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import ItemCount from "../ItemCount/ItemCount";
-
-
 import { FiShoppingCart } from "react-icons/fi";
+import {Link} from "react-router-dom";
 import { VscChevronLeft, VscChevronRight} from "react-icons/vsc";
+import "./itemDetail.css";
 
 export const ItemDetail = ({data}) => {
 	const [count, setCount] = useState(1);
+	const [goToCart, setGoToCart] = useState(false);
+
+	const onClickHandler = (e) => {
+		setGoToCart(true);
+	}
 
 	return(
-		<div className="product">
-			<div className="">
-				<img src={data.images[0]} alt={data.name} />
+		<div className="item-details__wrapper">
+			<div className="item-details__media">
+				<div className="id__media-container id__media-container--vista">
+					<img src={data.images[0]} alt={data.name} />
+				</div>	
+				<div className="id__media-container id__media-container--mini">
+					<div className="media-mini__item">
+						<img src={data.images[0]} alt={data.name} />
+					</div>
+					<div className="media-mini__item">
+						<img src={data.images[0]} alt={data.name} />
+					</div>
+				</div>	
 			</div>
-			<div className="">
-				<div className="">
+
+			<div className="item-details__info">
+				<div className="info__title">
 					<h1>{data.name}</h1>
 				</div>
-				<div className="price-and-info">
-					<span>${data.price}</span>
+				<div className="info__price-and-info">
+					<span className="item-details__price">${data.price}</span>
 				</div>
-				<div className="box-to-cart">
-					<ItemCount count={count} onCount={setCount} minCount={1} maxCount={data.stock}/>
-					<button>
-						<FiShoppingCart/>
-						<span>Agregar al carrito</span>
-					</button>
-				</div>
+				{!goToCart
+					? <div className="shipping__add-to-chart">
+						<ItemCount count={count} onCount={setCount} minCount={1} maxCount={data.stock}/>	
+						<button className="btn-add-to-chart" onClick={onClickHandler}><FiShoppingCart/><span> Comprar ahora</span></button>
+					</div>
+					: <div className="shipping__go-to-chart">
+						<Link className="shipping__go-to-chart__link" to="/cart">Finalizar compra</Link>
+					</div>
+				}
+				
 			</div>
 		</div>
 	);
